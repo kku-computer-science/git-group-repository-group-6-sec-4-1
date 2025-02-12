b6d5d8c6cb71299a4bc9bfdf0f72b8add3defdb9
phongwarin <phongwarin.k@kkumail.com>
2025-02-12 21:59:12 +0700

    Updated files and added LogController

--- InitialProject/Project_2-master/resources/views/dashboards/users/index.blade.php
+++ InitialProject/Project_2-master/resources/views/dashboards/users/index.blade.php
@@ -5,6 +5,6 @@
 
 <h3 style="padding-top: 10px;">ยินดีต้อนรับเข้าสู่ระบบจัดการข้อมูลวิจัยของสาขาวิชาวิทยาการคอมพิวเตอร์</h3>
 <br>
-<h4>สวัสดี {{Auth::user()->position_th}} {{Auth::user()->fname_th}} {{Auth::user()->lname_th}}</h2>
+<h4>สวัสดี {{ Auth::user()->position_th }} {{ Auth::user()->fname_th }} {{ Auth::user()->lname_th }}</h4>
 
 @endsection
