05af4370e86be85566c9f6c8fce69c2512bd6a2b
Tanawat Tanatka <tanawat.tan@kkumail.com>
2025-02-26 18:03:38 +0700

    Add system log table for continue working

--- InitialProject/Project_2-master/resources/views/dashboards/users/index.blade.php
+++ InitialProject/Project_2-master/resources/views/dashboards/users/index.blade.php
@@ -5,6 +5,56 @@
 
 <h3 style="padding-top: 10px;">ยินดีต้อนรับเข้าสู่ระบบจัดการข้อมูลวิจัยของสาขาวิชาวิทยาการคอมพิวเตอร์</h3>
 <br>
-<h4>สวัสดี {{Auth::user()->position_th}} {{Auth::user()->fname_th}} {{Auth::user()->lname_th}}</h2>
+<h4>สวัสดี {{ Auth::user()->position_th }} {{ Auth::user()->fname_th }} {{ Auth::user()->lname_th }}</h4>
+
+<div class="container mt-4">
+    <div class="row">
+        <!-- ตาราง Error Log -->
+        <div class="col-md-6">
+            <h5 class="text-danger">Error Logs</h5>
+            <table class="table table-bordered table-striped">
+                <thead class="table-danger">
+                    <tr>
+                        <th>#</th>
+                        <th>Message</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($errorLogs as $index => $log)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $log->message }}</td>
+                        <td>{{ $log->created_at }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <!-- ตาราง System Info Log -->
+        <div class="col-md-6">
+            <h5 class="text-primary">System Information Logs</h5>
+            <table class="table table-bordered table-striped">
+                <thead class="table-primary">
+                    <tr>
+                        <th>#</th>
+                        <th>Info</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($systemLogs as $index => $log)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $log->info }}</td>
+                        <td>{{ $log->created_at }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 @endsection
