daf4ed35fdff369a6e7cacf1443fb3c38d154ccc
N4mun <nutthapat.t@kkumail.com>
2025-03-12 01:37:58 +0700

    update dashboard

--- InitialProject/Source Code/app/Models/CriticalMessage.php
+++ InitialProject/Source Code/app/Models/CriticalMessage.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CriticalMessage extends Model
 {
     protected $fillable = [
-        'message', 'ip', 'url', 'email', 'user_agent', 'severity', 'timestamp', 'time_ago', 'is_dismissed'
+        'unique_key', 'message', 'ip', 'url', 'email', 'user_agent', 'severity', 'timestamp', 'time_ago', 'count', 'is_dismissed'
     ];
 
     protected $casts = [
